69c4e6c563b1139946dfad54872de7222d54cb63
megafayray <fay.brotherston@gmail.com>
2024-07-11 17:15:10 -0400

    Make grid 16x16 (was 4x4)

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ createDivs();
 
 function createDivs (){
     const container = document.querySelector("#container");
-    for (let i = 0; i <16; i++){
+    for (let i = 0; i < (16*16); i++){
         const div = document.createElement("div");
-        div.setAttribute("style", "background: black; width:15px; aspect-ratio: 1/1; box-sizing: border-box; border: dashed red")
+        div.setAttribute("style", "background: black; width:15px; aspect-ratio: 1/1; box-sizing: border-box; border: dashed red; flex: 1 0 6.25%;")
         div.classList.add("squares");
         container.appendChild(div);
     }
@@ -16,7 +16,18 @@ button.addEventListener("click", () => {
     if (enteredNumber >100 || 
         isNaN(enteredNumber)){
         alert("Please enter a number <= 100");
+    } else {
+        createNew(enteredNumber);
     };
 });
 
+function createNew(enteredNumber){
+    const container = document.querySelector("#container");
+    for (let i = 0; i < (enteredNumber * enteredNumber); i++){
+        const div = document.createElement("div");
+        div.setAttribute("style", "background: black; width:15px; aspect-ratio: 1/1; box-sizing: border-box; border: dashed red; flex: 1 0 calc(100 / enteredNumber);")
+        div.classList.add("squares");
+        container.appendChild(div);
+    }
+}
     
