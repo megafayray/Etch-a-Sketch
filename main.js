d845b981e374ccc575fef91a0cc1eabf1e728a52
megafayray <fay.brotherston@gmail.com>
2024-07-12 14:57:43 -0400

    Create new grid on button click.

--- main.js
+++ main.js
@@ -8,23 +8,26 @@ function createDivs (){
         div.classList.add("squares");
         container.appendChild(div);
     }
-}
+};
 
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     let enteredNumber = prompt("How many squares do you want?");
-    console.log(enteredNumber); //just to test that I'm getting the intended value
+    console.log(enteredNumber); //test = passed
     if (enteredNumber >100 || 
         isNaN(enteredNumber)){
         alert("Please enter a number <= 100");
     } else {
         const newContainer = document.querySelector("#container");
         let totalSquares = enteredNumber * enteredNumber;
-        console.log(totalSquares); //test 
+        console.log(totalSquares); //test = passed
         for (let i = 0; i < totalSquares; i++){
             
             const newDiv = document.createElement("div");
-            newDiv.setAttribute("style", "background: pink; width:15px; aspect-ratio: 1/1; box-sizing: border-box; border: dashed red; flex: 1 0 calc(100 / enteredNumber);")
+            newDiv.setAttribute("style", "background: pink; width:15px; aspect-ratio: 1/1; box-sizing: border-box; border: dashed red;")
+            let percentage = (100 / enteredNumber) + "%";
+            console.log(percentage); //test = passed
+            newDiv.style.flexBasis = percentage;
             newDiv.classList.add("newSquares");
             newContainer.appendChild(newDiv);
         }
